5abd728fd1d062713cd261987b8b221ccf242877
philpatt <phillip.s.patterson1@gmail.com>
2018-02-20 17:04:53 -0800

    created models, working on admin site views

--- mussels_site/admin.py
+++ mussels_site/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Team, Owner, Coach, Player, Program, Event, Location, Clinic, Schedule, Contact, Contact_Content, Partnership
+
 
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(Owner)
+admin.site.register(Coach)
+admin.site.register(Player)
+admin.site.register(Program)
+admin.site.register(Event)
+admin.site.register(Location)
+admin.site.register(Clinic)
+admin.site.register(Schedule)
+admin.site.register(Contact)
+admin.site.register(Contact_Content)
+admin.site.register(Partnership)
+
+
+
+
+
+
+
