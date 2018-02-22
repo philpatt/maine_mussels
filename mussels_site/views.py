1fa9c883fd2e47192a5e08d3beb550705d0e0426
philpatt <phillip.s.patterson1@gmail.com>
2018-02-21 16:48:27 -0800

    core pages, team pages, program pages

--- mussels_site/views.py
+++ mussels_site/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+# core page views
 def home(request):
     return render(request,'mussels_site/core/home.html')
+
+def contact(request):
+    return render(request,'mussels_site/core/contact.html')
+
+def about(request):
+    return render(request,'mussels_site/core/about.html')
+
+
+# team page views
+def green_team(request):
+    return render(request,'mussels_site/teams/green.html')
+
+def navy_team(request):
+    return render(request,'mussels_site/teams/navy.html')
+
+def futures_team(request):
+    return render(request,'mussels_site/teams/futures.html')
+
+
+# program and skills views
+def league(request):
+    return render(request, 'mussels_site/programs/league.html')
+
+def travel(request):
+    return render(request, 'mussels_site/programs/travel.html')
+
+def pick_up(request):
+    return render(request, 'mussels_site/programs/pick_up.html')
+
+def skills_clinics(request):
+    return render(request, 'mussels_site/programs/skills_clinics.html')
+
+
+    
